fe400d00632233d929c684a8d8039cec431add32
2022-0140 <2022-0140@LAB1-PC17>
2025-05-07 18:57:05 +0800

    Add project files.

---
+++ SirSoiProject/Controllers/StudentController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SirSoiProject.Models;
+
+namespace SirSoiProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static List<Student> students = new List<Student>();
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                student.StudentID = students.Count + 1;
+                students.Add(student);
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var student = students.FirstOrDefault(s => s.StudentID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Student student)
+        {
+            if (id != student.StudentID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var studentexisting = students.FirstOrDefault(s => s.StudentID == id);
+                if (studentexisting == null)
+                {
+                    return NotFound();
+                }
+
+                studentexisting.FirstName = student.FirstName;
+                studentexisting.MiddleName = student.MiddleName;
+                studentexisting.LastName = student.LastName;
+                studentexisting.YearLevel = student.YearLevel;
+                studentexisting.Gender = student.Gender;
+
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var student = students.FirstOrDefault(s => s.StudentID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost, ActionName ("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var student = students.FirstOrDefault(s => s.StudentID == id);
+            if (student != null)
+            {
+                students.Remove(student);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SirSoiProject/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace SirSoiProject.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string YearLevel { get; set; }
+        public string Gender { get; set; }
+    }
+}
